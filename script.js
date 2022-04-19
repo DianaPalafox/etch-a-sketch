d43cf35ffd44ac63b1ca6ead5bd24e8f42af15e8
Diana Palafox <dianapalafox95@gmail.com>
2022-04-19 12:09:47 -0500

    Add a button that clears the grid and send the user a popup asking for the number of squares per side for the new grid

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-let grid= 16*16;
 const container = document.querySelector('.container');
 
-function makeGrid() {
+function makeGrid(x) {
+    let grid= x*x;
     for (i=0; i<grid; i++) {
         const content = document.createElement('div');
         content.classList.add('content');
         content.style.border = "thin solid gray";
+        content.style.width = 600/x; 
+        content.style.height = 600/x; 
         container.appendChild(content); 
     }
 }
-makeGrid();
 
-let newContent = document.querySelectorAll('.content'); 
+makeGrid(16);
 
-newContent.forEach((content)=> {
+function colorBlack() {
+    let newContent = document.querySelectorAll('.content'); 
+    newContent.forEach((content)=> {
     content.addEventListener("mouseover", function() {
         content.style.backgroundColor = 'black';
-       
     });
 })
+}
 
-const button = document.querySelector('button'); 
-button.addEventListener('click', () => {
-    clearGrid();
-})
+colorBlack(); 
+
 
 function clearGrid() {
    let reset= document.querySelectorAll('.content'); 
    reset.forEach((content) => content.style.backgroundColor = 'white');
+   makeNewGrid();
+}
+
+function removeGrid() {
+    let newContent = document.querySelectorAll('.content'); 
+    newContent.forEach((content)=> {
+    content.remove();
+    });
+}
+
+
+function makeNewGrid(){
+    removeGrid(); 
+   let x=  Number(prompt("Please, select the size for your new grid. 100x100 is the max."));
+    if (x > 100) {
+        alert("Please select a new number that not surpases 100.")
+    }
+    else {
+        container.style.gridTemplateColumns = `repeat(${x},auto)`;
+        makeGrid(x);
+        colorBlack(); 
+    }
 }
